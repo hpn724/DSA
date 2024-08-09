5462e5b922c1cc115287c73b1c309f864676de7c
hpn724 <87013241+hpn724@users.noreply.github.com>
2024-08-09 11:37:19 +0530

    [885] [Time Beats: 42.36%] [Memory Beats: 25.56%] - LeetPush

--- Leetcode-problems/885-Spiral-Matrix-III.cpp
+++ Leetcode-problems/885-Spiral-Matrix-III.cpp
@@ -9,26 +9,21 @@ public:
         int c_curr = cStart;
 
         path.push_back({r_curr, c_curr});
-        cout<<"Adding : ["<<r_curr<<", "<<c_curr<<"]"<<endl;
-
+        
         c_curr += 1;
 
         while(path.size() < rows*cols)
         {
-            cout<<"Current step size : "<<curr_max_step<<endl;
             //move down
             
             if(r_curr == rStart - curr_max_step + 1 && c_curr == cStart + curr_max_step)
             {
-                cout<<"Moving down"<<endl;
                 while(r_curr < rStart + curr_max_step)
                 {
-                    cout<<"Current row : "<<r_curr<<" \tCurrent column : "<<c_curr<<endl;
                     if (c_curr<cols && r_curr>=0 && r_curr<rows && c_curr>=0)
                     {
                         path.push_back({r_curr, c_curr});
-                        cout<<"Adding : ["<<r_curr<<", "<<c_curr<<"]"<<endl;
-                    }
+                    }    
                     r_curr++;
                 }
 
@@ -38,15 +33,12 @@ public:
             if(r_curr == rStart + curr_max_step && c_curr == cStart + curr_max_step )
             {
                 
-                cout<<"Moving left"<<endl;
                 while(c_curr > cStart - curr_max_step)
                 {
-                    cout<<"Current row : "<<r_curr<<" \tCurrent column : "<<c_curr<<endl;
                     if(c_curr<cols && r_curr>=0 && r_curr<rows && c_curr>=0)
                     {
                         path.push_back({r_curr, c_curr});
-                        cout<<"Adding : ["<<r_curr<<", "<<c_curr<<"]"<<endl;
-                    }
+                    }    
                     c_curr--;
                 }
             }
@@ -56,16 +48,13 @@ public:
             //move up
             if(r_curr == rStart + curr_max_step && c_curr == cStart - curr_max_step )
             {
-                cout<<"Moving up"<<endl;
                 
                 while(r_curr > rStart - curr_max_step)
                 {
-                    cout<<"Current row : "<<r_curr<<" \tCurrent column : "<<c_curr<<endl;
                     if(c_curr<cols && r_curr>=0 && r_curr<rows && c_curr>=0)
                     {
                         path.push_back({r_curr, c_curr});
-                        cout<<"Adding : ["<<r_curr<<", "<<c_curr<<"]"<<endl;
-                    }
+                    } 
                     r_curr--;
                 }
             }
@@ -74,15 +63,12 @@ public:
             if(r_curr == rStart - curr_max_step && c_curr == cStart - curr_max_step)
             {
                 
-                cout<<"Moving right"<<endl;
                 curr_max_step++;
                 while(c_curr < cStart + curr_max_step)
                 {
-                    cout<<"Current row : "<<r_curr<<" \tCurrent column : "<<c_curr<<endl;
                     if(c_curr<cols && r_curr>=0 && r_curr<rows && c_curr>=0 )
                     {
                         path.push_back({r_curr, c_curr});
-                        cout<<"Adding : ["<<r_curr<<", "<<c_curr<<"]"<<endl;
                     }
                     c_curr++;
                 }
